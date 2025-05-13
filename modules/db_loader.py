324648a0c2a7869d3f590585b7647aaf40dfff66
shoh90 <52881023+shoh90@users.noreply.github.com>
2025-05-13 20:20:47 +0900

    Delete modules directory

--- modules/db_loader.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-
-def load_asos_data(db_path):
-    conn = sqlite3.connect(db_path)
-    df_weather = pd.read_sql("SELECT * FROM asos_weather", conn)
-    df_sunshine = pd.read_sql("SELECT * FROM sunshine_data", conn)
-    conn.close()
-    return df_weather, df_sunshine
--- modules/preprocessing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def preprocess_data(df_weather, df_sunshine):
-    df_weather['일시'] = pd.to_datetime(df_weather['일시'])
-    df_sunshine['일시'] = pd.to_datetime(df_sunshine['일시'])
-    df_merge = pd.merge(df_weather, df_sunshine, on=['지점명', '일시'], how='left')
-    return df_merge
